93222bd84f313768bc46b9a6816c27e4b430daa6
JNRivadeneira <jnrivadeneira@gmail.com>
2023-01-20 16:21:46 -0300

    Agrega una vista de login.

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,58 +1,37 @@
 <script setup>
-// import { Vueform, useVueform } from "@vueform/vueform";
 
-// export default {
-//   name: 'LoginForm',
-//   mixins: [Vueform],
-//   setup: useVueform,
-//   data() {
-//     return {
-//       datosLogin: {},
-//     };
-//   },
-  
-//   methods: {
-//     chequearUsuario() {
-//       console.log("Se ejecuta chequearUsuario()");
-//       const encontrado = this.$store.state.listaUsuarios.find(
-//         (usuario) =>
-//           usuario.email == this.datosLogin.email &&
-//           usuario.password == this.datosLogin.password
-//       );
-//       console.log(`encontrado es: ${encontrado}`);
-//     },
-//   },
-// };
 </script>
 
 <template>
-  <h1>Login Form</h1>
-  <!-- <Vueform v-model="datosLogin" endpoint="get_user">
-    <StaticElement name="titulo"><h1>Login</h1></StaticElement>
-    <GroupElement name="informacionPersonal" label="">
-      <TextElement
-        name="email"
-        placeholder="Email"
-        rules="required|email|max:255"
-        :debounce="300"
-        validate-on="change"
-      />
-      <TextElement
-        name="password"
-        input-type="password"
-        placeholder="Contraseña"
-        :rules="['required']"
-        :debounce="300"
-      />
-    </GroupElement>
-    <ButtonElement name="crearUsuario" add-class="mt-2" submits>
-      Ingresar
-    </ButtonElement>
-    <StaticElement name="linkToLogin">
-      <router-link to="/registro">No tengo una cuenta</router-link>
-      <pre>{{datosLogin}}</pre>
-    </StaticElement>
-  </Vueform> -->
+  <div class="relative flex flex-col justify-center min-h-screen overflow-hidden">
+    <div class="w-full p-6 m-auto bg-white rounded shadow-lg ring-2 ring-purple-800/50 lg:max-w-md">
+        <h1 class="text-3xl font-semibold text-center text-purple-700">LOGIN</h1>
+
+        <form class="mt-6">
+            <div>
+                <label for="email" class="block text-sm text-gray-800">Email</label>
+                <input type="email"
+                    class="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40">
+            </div>
+            <div class="mt-4">
+                <div>
+                    <label for="password" class="block text-sm text-gray-800">Password</label>
+                    <input type="password"
+                        class="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40">
+                </div>
+                <a href="#" class="text-xs text-gray-600 hover:underline">Forget Password?</a>
+                <div class="mt-6">
+                    <button
+                        class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
+                        Login
+                    </button>
+                </div>
+            </div>
+        </form>
+        <p class="mt-8 text-xs font-light text-center text-gray-700"> Si aún no eres cliente <a href="#"
+                class="font-medium text-purple-600 hover:underline">Registrate</a></p>
+    </div>
+</div>
 </template>
 
 <style scoped>
