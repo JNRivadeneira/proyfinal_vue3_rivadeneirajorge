d748625a96e4d87a1332dc742929d8b9a01b1930
JNRivadeneira <jnrivadeneira@gmail.com>
2023-01-25 14:27:36 -0300

    Agrega componente y vista para registro de usuario

---
+++ src/views/RegisterFormView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import RegisterNewUserForm from '../components/forms/RegisterNewUserForm.vue';
+// import RegistroForm from '../components/RegistroForm.vue'
+// export default {
+//     components: { RegistroForm },
+//     name: 'RegistroView'
+// }
+</script>
+
+<template>
+  <RegisterNewUserForm />
+  <!-- <div class="Registro">
+    <registro-form/>
+  </div> -->
+</template>
