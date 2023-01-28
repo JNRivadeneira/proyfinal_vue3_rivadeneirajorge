b616fb9a6bf679bc02267c900caf048ae9aa7241
JNRivadeneira <jnrivadeneira@gmail.com>
2023-01-27 21:53:16 -0300

    Agrega método getActiveUser al store de usuarios

--- src/stores/UserStore.js
+++ src/stores/UserStore.js
@@ -5,15 +5,15 @@ import users from '../data/users.json'
 export const useUserStore = defineStore('UserStore', () => {
   // state -> ref()
   const registeredUsers = ref([])
-  const activeUser = ref([])
+  const activeUser = ref(null)
 
   // getters -> computed()
-  const getUsers = computed(() => { return registeredUsers.value})
-
+  const getUsers = computed(() => { return registeredUsers.value })
+  const getActiveUser = computed(() => { return activeUser.value })
   // actions -> function()
   function loadUsers() { registeredUsers.value = users}
   function setActiveUser(user) { activeUser.value = user }
 
   // return
-  return { registeredUsers, activeUser, getUsers, loadUsers, setActiveUser}
+  return { registeredUsers, activeUser, getUsers, getActiveUser, loadUsers, setActiveUser}
 })
