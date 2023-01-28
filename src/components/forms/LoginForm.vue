2c0aec4bba53642c22beda64879db05cec77198c
Jorge Rivadeneira <jnrivadeneira@gmail.com>
2023-01-27 23:23:18 -0300

    30 agregar funcionalidad de inicio de sesión (#33)
    * Crea el userstore
    
    * Reconoce usuario activo y lo mantiene en store
    
    * Agrega condición if para mostrar navbar
    
    * Agrega método getActiveUser al store de usuarios
    
    * Agrega funcionalidad de inicio de sesión al form
    
    * Agrega funcionalidad de inicio de sesión

--- src/components/forms/LoginForm.vue
+++ src/components/forms/LoginForm.vue
@@ -1,5 +1,22 @@
 <script setup>
+import { useUserStore } from '../../stores/UserStore';
 
+const userStore = useUserStore()
+userStore.loadUsers()
+const registeredUsers = userStore.getUsers
+// const found = ref({})
+
+const whoIsLogging = {
+  email: "",
+  password: ""
+}
+const validateUser = (whoIsLogging) => {
+  console.log("Se presionó el botón")  
+  const found = registeredUsers.find((registeredUser) => (registeredUser.email == whoIsLogging.email && registeredUser.password == whoIsLogging.password))
+  if (found) {
+    userStore.setActiveUser(found.nombre)
+  }
+}
 </script>
 
 <template>
@@ -7,21 +24,26 @@
     <div class="w-full p-6 m-auto bg-white rounded shadow-lg ring-2 ring-purple-800/50 lg:max-w-md">
         <h1 class="text-3xl font-semibold text-center text-purple-700">LOGIN</h1>
 
-        <form class="mt-6">
+        <form @submit.prevent="validateUser(whoIsLogging)" class="mt-6">
             <div>
                 <label for="email" class="block text-sm text-gray-800">Email</label>
-                <input type="email"
+                <input
+                    v-model="whoIsLogging.email" 
+                    type="email"
                     class="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40">
             </div>
             <div class="mt-4">
                 <div>
                     <label for="password" class="block text-sm text-gray-800">Password</label>
-                    <input type="password"
+                    <input
+                    v-model="whoIsLogging.password" 
+                        type="password"
                         class="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40">
                 </div>
                 <a href="#" class="text-xs text-gray-600 hover:underline">Forget Password?</a>
                 <div class="mt-6">
                     <button
+                        
                         class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
                         Login
                     </button>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,15 @@
 <script setup>
+import { useUserStore } from '../stores/UserStore'
 import HomeComponent from '../components/HomeComponent.vue';
+import LandingPageComponent from '../components/LandingPageComponent.vue';
+import LoginForm from '../components/forms/LoginForm.vue';
+
+const userStore = useUserStore()
+
 </script>
 
 <template>
-    <HomeComponent />
+    <HomeComponent v-if="userStore.getActiveUser" :active-user="userStore.getActiveUser"/>
+    <!-- <LandingPageComponent /> -->
+    <LoginForm v-else=""/>
 </template>
