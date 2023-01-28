9a08ef164eb00d999d4877174ccb3db667e1dad9
JNRivadeneira <jnrivadeneira@gmail.com>
2023-01-27 21:52:05 -0300

    Agrega condición if para mostrar navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
+import { useUserStore } from './stores/UserStore';
 import NavbarComponentVue from './components/NavbarComponent.vue';
+
+const userStore = useUserStore()
 </script>
 
 <template>
-  <div>
+  <div v-if="userStore.getActiveUser">
     <navbar-component-vue />
   </div>
   <router-view></router-view>
