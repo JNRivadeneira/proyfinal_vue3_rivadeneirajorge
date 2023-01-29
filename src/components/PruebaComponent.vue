f055436bc8352a977ede94aa17e39b4b7ca0dabd
JNRivadeneira <jnrivadeneira@gmail.com>
2023-01-29 17:40:46 -0300

    Agrega 'lodash' para crear getter de cartStore

--- src/components/PruebaComponent.vue
+++ src/components/PruebaComponent.vue
@@ -1,20 +1,9 @@
 <script setup>
-import ProductCard from './ProductCard.vue';
-
-const producto = [
-  {
-    "title": "Tablas de Quesos",
-    "image": "https://i.postimg.cc/GmTgrwS6/tabla-Quesos.jpg",
-    "price": 10,
-    "description": "Sunt exercitation officia elit qui officia do ut sunt ullamco incididunt ipsum pariatur cillum. Labore aliqua ipsum incididunt in elit minim duis adipisicing pariatur. Nulla magna in elit Lorem mollit non cillum commodo nostrud laborum consectetur.",
-    "shoppingCard": true,
-    "availableItems": 3,
-  }
-]
+import CartWidget from './pruebas/CartWidget.vue'
 </script>
 
 <template>
-  <ProductCard :product="producto"/>
+ <CartWidget />
 </template>
 
 <style></style>
--- src/stores/CartStore.js
+++ src/stores/CartStore.js
@@ -1,4 +1,5 @@
 import { defineStore } from "pinia";
+import { groupBy } from "lodash";
 import { ref, computed } from "vue";
 
 export const useCartStore = defineStore("CartStore", () => {
@@ -7,6 +8,7 @@ const items = ref([])
 // getters -> computed()
 const getItemsCount = computed(() => {return items.value.length})
 const isEmpty = computed(() => { return items.value.length === 0 })
+const grouped = computed(() => { return groupBy(items.value, (item) => item.title)})
 // actions -> funtion()
 function addItems (count, item) {
   count = parseInt(count)
@@ -17,5 +19,5 @@ function addItems (count, item) {
 
 function removeAllItems() { items.value = []}
 // return
-return {items, addItems, getItemsCount, removeAllItems, isEmpty}
+return {items, addItems, getItemsCount, removeAllItems, isEmpty, grouped}
 })
