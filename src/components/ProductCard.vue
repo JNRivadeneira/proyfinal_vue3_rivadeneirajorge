79139b8a57223f120039def1c16657ea4514f518
JNRivadeneira <jnrivadeneira@gmail.com>
2023-01-29 14:16:39 -0300

    Elimina hrefs a # y modifica gitignore

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -21,11 +21,11 @@ const count = ref(0)
 <template>
   <div id="card"
     class="flex flex-col min-w-300 max-h-max bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
+    <a>
       <img class="w-full h-52 rounded-t-lg object-cover" :src="product.image" alt="product image" />
     </a>
     <div class="flex flex-col px-5 pb-5">
-      <a href="#">
+      <a>
         <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{{ product.title }}</h5>
       </a>
       <div v-if="product.rating">
@@ -35,7 +35,8 @@ const count = ref(0)
         <div id="product_price" class="text-2xl font-bold text-gray-900 dark:text-white">AR$
           {{ product.price }}</div>
         <AppCountInput v-model="count" :available-items="product.availableItems" />
-        <a title="Agregar al carrito" href="#"
+        <a 
+          title="Agregar al carrito"
           class="flex justify-center items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-b-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           @click="$emit('addToCart', count), (count = 0)">
           <ShoppingCartIcon class="h-7 w-7 text-blue-200" />
