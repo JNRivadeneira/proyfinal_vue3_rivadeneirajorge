14eed6f759a505837f759b4a5da0480da60f5714
JNRivadeneira <jnrivadeneira@gmail.com>
2023-01-30 13:04:46 -0300

    Elimina carritoComponent.

--- src/router/index.js
+++ src/router/index.js
@@ -18,14 +18,6 @@ const router = createRouter({
       component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
     },
     {
-      path: '/carrito',
-      name: 'carrito',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "carrito" */ '../views/CarritoView.vue')
-    },
-    {
       path: '/menu/entradas',
       name: 'menuEntradas',
       // route level code-splitting
