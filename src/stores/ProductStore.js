5de15968193270221cf6def71dc5126f04af5f3f
JNRivadeneira <jnrivadeneira@gmail.com>
2023-01-31 17:51:22 -0300

    Cambia la función fill y la url de API.

--- src/stores/ProductStore.js
+++ src/stores/ProductStore.js
@@ -1,10 +1,29 @@
+import { ref } from "vue";
 import { defineStore } from "pinia";
 import axios from "axios";
 // import products from '../data/products.json'
 
+const URLGET_ENTRADAS = "https://api.jsonbin.io/v3/b/63d96ffdace6f33a22d1b85d?meta=false"
+const URLPUT_ENTRADAS = "https://api.jsonbin.io/v3/b/63d96ffdace6f33a22d1b85d"
+
+const REQ_CONFIG = {
+  headers: {"X-Master-Key": "$2b$10$ZyKyROpXJIjQJK8kHryPPu9LtG53TPB5A3DxYfZUzlHnsxo06F7vK", "Content-Type": "application/json"}
+}
+
+export const useProductStore = defineStore('ProductStore', () => {
+  // state
+  const entradas = ref([])
+
+  // actions
+  async function fill(){
+    entradas.value = (await axios.get(URLGET_ENTRADAS, REQ_CONFIG)).data
+  }
+
+  return {entradas, fill}
+})
 
 
-export const useProductStore = defineStore('ProductStore', {
+/*
   // state
   state: () => {
     return {
@@ -20,3 +39,4 @@ export const useProductStore = defineStore('ProductStore', {
   }
   // getters
 })
+*/
