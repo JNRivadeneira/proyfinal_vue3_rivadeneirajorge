b60c5693490f8bb37667c379072dc3b2379f3a1c
JNRivadeneira <jnrivadeneira@gmail.com>
2023-01-31 17:58:34 -0300

    Agrega espacio en blanco al final del archivo.

--- src/stores/ProductStore.js
+++ src/stores/ProductStore.js
@@ -21,22 +21,3 @@ export const useProductStore = defineStore('ProductStore', () => {
 
   return {entradas, fill}
 })
-
-
-/*
-  // state
-  state: () => {
-    return {
-      products: {}
-    };
-  },
-  // actions
-  actions: {
-    async fill(){
-      let URLGET="https://6384e61b3fa7acb14f041a45.mockapi.io/api/platos";
-      this.products = (await axios.get(URLGET)).data
-    }
-  }
-  // getters
-})
-*/
